16bc6825b77742aced6ae747d91d94867f3190bd
AlifSakib <alifsakib@gmail.com>
2022-11-12 15:16:20 +0600

    added images to services section

--- src/Pages/Home/InfoSection.js
+++ src/Pages/Home/InfoSection.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import image1 from "../../assets/servicesImage/About.jpeg";
 const InfoSection = () => {
   return (
     <div>
@@ -11,7 +11,7 @@ const InfoSection = () => {
               <div class="relative h-64 sm:h-80 lg:h-full">
                 <img
                   alt="House"
-                  src="https://images.unsplash.com/photo-1583608205776-bfd35f0d9f83?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                  src={image1}
                   class="absolute inset-0 h-full w-full object-cover"
                 />
               </div>
--- src/Pages/Home/Services.js
+++ src/Pages/Home/Services.js
@@ -1,43 +1,58 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import image1 from "../../assets/servicesImage/image1.webp";
+import image2 from "../../assets/servicesImage/image2.jpeg";
+import image3 from "../../assets/servicesImage/imagw3.webp";
 const Services = () => {
   return (
     <div className=" px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="text-6xl mb-12 underline underline-offset-4 text-light-blue-900">
         <h1>Our Services</h1>
       </div>
-      <div className=" grid grid-cols-3  gap-6">
+      <div className=" grid lg:grid-cols-3  gap-6">
         <Link to="#" className="group relative block h-96">
           <span className="absolute inset-0 border-2 border-dashed border-black"></span>
 
           <div className="relative flex h-full transform items-end border-2 border-black bg-white transition-transform group-hover:-translate-x-2 group-hover:-translate-y-2">
             <div className="px-8 pb-8 transition-opacity group-hover:absolute group-hover:opacity-0">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-12 w-12"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
+              <img
+                className="absolute h-full object-cover top-0 left-0 w-full"
+                src={image1}
+                alt=""
+              />
+              <div class="absolute inset-0 bg-white/75 sm:bg-transparent sm:bg-gradient-to-t sm:from-black/75 sm:to-black/10"></div>
+
+              <h2 className="relative mt-4 text-2xl font-medium text-white">
+                <svg
+                  className="w-10 mb-3"
+                  viewBox="0 0 24 24"
                   strokeLinejoin="round"
                   strokeWidth="2"
-                  d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-
-              <h2 className="mt-4 text-2xl font-medium">Go around the world</h2>
+                  strokeLinecap="round"
+                  strokeMiterlimit="10"
+                  stroke="currentColor"
+                  fill="none"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                Wellness & Vacination
+              </h2>
             </div>
 
-            <div className="absolute p-8 opacity-0 transition-opacity group-hover:relative group-hover:opacity-100">
-              <h3 className="mt-4 text-2xl font-medium">Go around the world</h3>
+            <div className="absolute p-8 opacity-0 transition-opacity group-hover:relative group-hover:opacity-100 overflow-hidden">
+              <h3 className="mt-4 text-2xl font-medium overflow-hidden">
+                Wellness & Vacination
+              </h3>
 
-              <p className="mt-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Cupiditate, praesentium voluptatem omnis atque culpa
-                repellendus.
+              <p className="mt-4 overflow-hidden">
+                The WCVM Veterinary Medical Centre's Wellness Service offers
+                vaccinations within wellness visits or through scheduled
+                vaccination clinics that are frequently offered on Saturdays at
+                the Small Animal Clinic. Contact us for more information.
               </p>
 
               <p className="mt-8 font-bold">Read more</p>
@@ -49,31 +64,43 @@ const Services = () => {
 
           <div className="relative flex h-full transform items-end border-2 border-black bg-white transition-transform group-hover:-translate-x-2 group-hover:-translate-y-2">
             <div className="px-8 pb-8 transition-opacity group-hover:absolute group-hover:opacity-0">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-12 w-12"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
+              <img
+                className="absolute h-full object-cover top-0 left-0 w-full"
+                src={image2}
+                alt=""
+              />
+              <div class="absolute inset-0 bg-white/75 sm:bg-transparent sm:bg-gradient-to-t sm:from-black/75 sm:to-black/10"></div>
+
+              <h2 className="relative mt-4 text-2xl font-medium text-white">
+                <svg
+                  className="w-10 mb-3"
+                  viewBox="0 0 24 24"
                   strokeLinejoin="round"
                   strokeWidth="2"
-                  d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-
-              <h2 className="mt-4 text-2xl font-medium">Go around the world</h2>
+                  strokeLinecap="round"
+                  strokeMiterlimit="10"
+                  stroke="currentColor"
+                  fill="none"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                Surgery
+              </h2>
             </div>
 
             <div className="absolute p-8 opacity-0 transition-opacity group-hover:relative group-hover:opacity-100">
-              <h3 className="mt-4 text-2xl font-medium">Go around the world</h3>
+              <h3 className="mt-4 text-2xl font-medium">Surgery</h3>
 
               <p className="mt-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Cupiditate, praesentium voluptatem omnis atque culpa
-                repellendus.
+                Veterinary surgery is surgery performed on animals by
+                veterinarians, whereby the procedures fall into three broad
+                categories: orthopaedics (bones, joints, muscles), soft tissue
+                surgery (skin, body cavities, cardiovascular system,
+                GI/urogenital/respiratory tracts), and neurosurgery.
               </p>
 
               <p className="mt-8 font-bold">Read more</p>
@@ -85,31 +112,43 @@ const Services = () => {
 
           <div className="relative flex h-full transform items-end border-2 border-black bg-white transition-transform group-hover:-translate-x-2 group-hover:-translate-y-2">
             <div className="px-8 pb-8 transition-opacity group-hover:absolute group-hover:opacity-0">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-12 w-12"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
+              <img
+                className="absolute h-full object-cover top-0 left-0 w-full"
+                src={image3}
+                alt=""
+              />
+              <div class="absolute inset-0 bg-white/75 sm:bg-transparent sm:bg-gradient-to-t sm:from-black/75 sm:to-black/10"></div>
+
+              <h2 className="relative mt-4 text-2xl font-medium text-white">
+                <svg
+                  className="w-10 mb-3"
+                  viewBox="0 0 24 24"
                   strokeLinejoin="round"
                   strokeWidth="2"
-                  d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-
-              <h2 className="mt-4 text-2xl font-medium">Go around the world</h2>
+                  strokeLinecap="round"
+                  strokeMiterlimit="10"
+                  stroke="currentColor"
+                  fill="none"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                Dentistry
+              </h2>
             </div>
 
             <div className="absolute p-8 opacity-0 transition-opacity group-hover:relative group-hover:opacity-100">
-              <h3 className="mt-4 text-2xl font-medium">Go around the world</h3>
+              <h3 className="mt-4 text-2xl font-medium">Dentistry</h3>
 
-              <p className="mt-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Cupiditate, praesentium voluptatem omnis atque culpa
-                repellendus.
+              <p className="mt-4 overflow-hidden">
+                Veterinary dentistry is the field of dentistry applied to the
+                care of animals. It is the art and science of prevention,
+                diagnosis, and treatment of conditions, diseases, and disorders
+                of the oral cavity, the maxillofacial region, and its associated
+                structures as it relates to animals.
               </p>
 
               <p className="mt-8 font-bold">Read more</p>
