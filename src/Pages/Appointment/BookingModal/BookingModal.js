6563be35b820bad0edc168d8c489d367824348ca
AlifSakib <alifsakib@gmail.com>
2022-11-13 01:43:45 +0600

    added modal for every appointment

--- src/Pages/Appointment/BookingModal/BookingModal.js
+++ src/Pages/Appointment/BookingModal/BookingModal.js
@@ -1,14 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const BookingModal = ({ handleModal, modal, option }) => {
   const { name, slots } = option;
   return (
     <div>
-      <div
-        id="booking-modal"
-        class="rounded-xl border  border-gray-100 p-4 shadow-xl bg-blue-900"
-      >
+      <div id="booking-modal" class="rounded-xl  p-4 shadow-xl bg-blue-900">
         <div class="flex items-start gap-4">
           <span class="text-green-600">
             <svg
@@ -34,34 +30,106 @@ const BookingModal = ({ handleModal, modal, option }) => {
             </strong>
 
             <p class="mt-1 text-sm text-white">{name}</p>
-
-            <div class="mt-4 flex gap-2">
-              <Link
-                to="#"
-                class="inline-flex items-center gap-2 rounded-lg bg-blue-500 px-4 py-2 text-white transition hover:bg-blue-600"
-              >
-                <span class="text-sm"> Preview </span>
-
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="h-4 w-4"
+            <form>
+              <div className="mb-2">
+                <label
+                  for="UserEmail"
+                  class="block text-xs font-medium text-white mt-1 "
                 >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
-                  />
-                </svg>
-              </Link>
+                  Email
+                </label>
 
-              <button class="block rounded-lg px-4 py-2 transition hover:bg-gray-100 text-white">
-                <span class="text-sm">Revert</span>
-              </button>
-            </div>
+                <input
+                  type="email"
+                  id="UserEmail"
+                  placeholder="john@rhcp.com"
+                  class="mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
+                />
+              </div>
+              <div className="mb-2">
+                <label
+                  for="UserEmail"
+                  class="block text-xs font-medium text-white"
+                >
+                  Email
+                </label>
+
+                <input
+                  type="email"
+                  id="UserEmail"
+                  placeholder="john@rhcp.com"
+                  class="mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
+                />
+              </div>
+              <div className="mb-2">
+                <label
+                  for="UserEmail"
+                  class="block text-xs font-medium text-white"
+                >
+                  Email
+                </label>
+
+                <input
+                  type="email"
+                  id="UserEmail"
+                  placeholder="john@rhcp.com"
+                  class="mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
+                />
+              </div>
+              <div className="mb-2">
+                <label
+                  for="UserEmail"
+                  class="block text-xs font-medium text-white"
+                >
+                  Email
+                </label>
+
+                <input
+                  type="email"
+                  id="UserEmail"
+                  placeholder="john@rhcp.com"
+                  class="mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
+                />
+              </div>
+              <div className="mb-2">
+                <label
+                  for="UserEmail"
+                  class="block text-xs font-medium text-white"
+                >
+                  Email
+                </label>
+
+                <input
+                  type="email"
+                  id="UserEmail"
+                  placeholder="john@rhcp.com"
+                  class="mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
+                />
+              </div>
+              <div class="mt-4 flex gap-2 ">
+                <button
+                  to="#"
+                  class="inline-flex items-center gap-2 rounded-lg bg-blue-500 px-4 py-2 text-white transition hover:bg-blue-600 w-full "
+                >
+                  <span class="text-sm "> Submit </span>
+
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="h-4 w-4"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </form>
           </div>
 
           <button
--- tailwind.config.js
+++ tailwind.config.js
@@ -325,5 +325,5 @@ module.exports = {
     opacity: ["responsive", "hover", "focus", "group-hover"],
     backgroundColor: ["responsive", "hover", "focus", "group-hover"],
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
