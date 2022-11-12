457ed95643d76eb00676b67bd9dc045885641a7e
AlifSakib <alifsakib@gmail.com>
2022-11-12 12:49:56 +0600

    added testimonials

--- src/Pages/Home/Testimonial.js
+++ src/Pages/Home/Testimonial.js
@@ -1,39 +1,13 @@
-import React, { useState } from "react";
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-
-import "./Testimonial.css";
-
-// import required modules
-import { Navigation, Pagination } from "swiper";
+import React from "react";
+import Reviews from "./Reviews";
 
 const Testimonial = () => {
-  const [setSwiperRef] = useState(null);
-
   return (
-    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-      <Swiper
-        onSwiper={setSwiperRef}
-        slidesPerView={3}
-        centeredSlides={true}
-        spaceBetween={30}
-        pagination={{
-          type: "fraction",
-        }}
-        navigation={true}
-        modules={[Pagination, Navigation]}
-        className="mySwiper"
-      >
-        <SwiperSlide>Nice Slide</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-      </Swiper>
+    <div className="grid grid-cols-4">
+      <Reviews></Reviews>
+      <Reviews></Reviews>
+      <Reviews></Reviews>
+      <Reviews></Reviews>
     </div>
   );
 };
