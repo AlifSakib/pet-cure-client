c3220a4b0bd49e03d72bf4257f8478cd2eb245ba
AlifSakib <alifsakib@gmail.com>
2022-11-12 22:55:42 +0600

    added appointment options data from local

---
+++ src/Pages/Appointment/AvailableAppointments/AppointmentOption.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const AppointmentOption = ({ option }) => {
+  console.log(option);
+  return <div>Options</div>;
+};
+
+export default AppointmentOption;
--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -1,12 +1,27 @@
 import { format } from "date-fns";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import AppointmentOption from "./AppointmentOption";
 
 const AvailableAppointments = ({ selected }) => {
+  const [options, setOptions] = useState([]);
+  useEffect(() => {
+    fetch("appointmentsOptions.json")
+      .then((res) => res.json())
+      .then((data) => setOptions(data));
+  }, []);
   return (
     <div className="my-16">
       <p className="text-center text-2xl text-blue-900">
         Available Appointments on {format(selected, "PP")}
       </p>
+      <div>
+        {options.map((option) => (
+          <AppointmentOption
+            key={option._id}
+            option={option}
+          ></AppointmentOption>
+        ))}
+      </div>
     </div>
   );
 };
