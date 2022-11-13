8f47256796b020e8dc91233bea1bfdfe8642a48f
AlifSakib <alifsakib@gmail.com>
2022-11-13 18:41:16 +0600

    added login form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-  return <div>Login</div>;
+  const { register, handleSubmit } = useForm();
+  const [data, setData] = useState("");
+  return (
+    <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+      <div class="mx-auto max-w-lg text-center">
+        <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+          <input
+            className="block w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm mb-4 border"
+            {...register("firstName")}
+            placeholder="First name"
+          />
+          <input
+            className="block w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm mb-4 border"
+            {...register("firstName")}
+            placeholder="First name"
+          />
+          <input
+            className=" ml-3 inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white"
+            type="submit"
+          />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
