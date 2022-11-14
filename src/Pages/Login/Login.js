a0bb0eed517137001aa14b39f6db26b6426faf3d
AlifSakib <alifsakib@gmail.com>
2022-11-14 13:14:25 +0600

    added react hook signup form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,7 +3,11 @@ import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const Login = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const handleLogin = (data) => {
     console.log(data);
@@ -16,14 +20,19 @@ const Login = () => {
           <input
             className="block w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm mb-4 border"
             type="email"
-            {...register("email")}
+            {...register("email", { required: "Email Address Is Required" })}
           />
+          {errors.email && <p role="alert">{errors.email?.message}</p>}
           <h1 className="text-start text-gray-700">Password</h1>
           <input
             className="block w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm mb-4 border"
             type="password"
-            {...register("password")}
+            {...register("password", {
+              required: "Password Is Required",
+              minLength: { value: 6, message: "Must be 6 character" },
+            })}
           />
+          {errors.password && <p role="alert">{errors.password?.message}</p>}
           <input
             className="  inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white w-full cursor-pointer"
             type="submit"
